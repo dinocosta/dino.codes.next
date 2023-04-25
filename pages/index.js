f63c753d8b2fd26904e7104507a947cba59252a6
dino <dinojoaocosta@gmail.com>
2023-04-25 20:34:31 +0100

    Trying to fix index page on mobile devices

--- pages/index.js
+++ pages/index.js
@@ -6,12 +6,14 @@ const jakarta = Plus_Jakarta_Sans({ subsets: ['latin'] });
 
 export default function Home() {
   return (
-		<div className={`flex min-h-screen items-end ${jakarta.className}`}>
-			<div className="mb-auto"></div>
-			<div className="m-8">
-				<Link href="/" className="link">Posts</Link>
-				<Link href="/" className="link">Sketches</Link>
-				<Link href="/" className="link">About</Link>
+		<div className="absolute inset-0">
+			<div className={`flex min-h-screen items-end ${jakarta.className}`}>
+				<div className="mb-auto"></div>
+				<div className="m-20">
+					<Link href="/" className="link">Posts</Link>
+					<Link href="/sketches" className="link">Sketches</Link>
+					<Link href="/" className="link">About</Link>
+				</div>
 			</div>
 		</div>
   );
