79d34e8395acce1c02a3296fa7518976db044191
dino <dinojoaocosta@gmail.com>
2023-04-29 15:41:03 +0100

    Fix build

--- components/layout.js
+++ components/layout.js
@@ -1,15 +1,15 @@
-import Head from 'next/head'
-import { Plus_Jakarta_Sans } from 'next/font/google';
+import Head from "next/head";
+import { Plus_Jakarta_Sans } from "next/font/google";
 
-const jakarta = Plus_Jakarta_Sans({ subsets: ['latin'] });
+const jakarta = Plus_Jakarta_Sans({ subsets: ["latin"] });
 
 export default function Layout({ children }) {
-	return ([
-		<Head>
-			<title>dino.codes</title>
-		</Head>,
-		<div className={`container mx-auto bg-light-gray ${jakarta.className}`}>
-			{children}
-		</div>
-	])
+  return (
+    <div className={`bg-light-gray container mx-auto ${jakarta.className}`}>
+      <Head>
+        <title>dino.codes</title>
+      </Head>
+      {children}
+    </div>
+  );
 }
