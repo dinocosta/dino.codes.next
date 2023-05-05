e042a12cfef9723a390742d1c1454423795b15b0
dino <dinojoaocosta@gmail.com>
2023-05-06 00:05:35 +0100

    Add more sketches

--- components/sketch_preview.js
+++ components/sketch_preview.js
@@ -2,14 +2,19 @@ import Image from 'next/image'
 import Link from 'next/link'
 
 export default function SketchPreview({ id, image, description }) {
-	return [
-		<div key={`${id}_image`} className='relative w-full aspect-square col-span-1 mb-10'>
-			<Image src={image} fill='true' className='rounded-md' alt='Sketch preview.'/>
-		</div>,
-		<div key={`${id}_description`} className='flex flex-col col-span-2 px-10 mb-10'>
-			<Link href={`/sketches/${id}`} className='mt-auto'>
-				<h2 className='font-medium text-xl'>Sketch {id}</h2>
-			</Link>
-		</div>
-	]
+  return (
+    <Link href={`/sketches/${id}`} className="mt-auto">
+      <div
+        key={`${id}_image`}
+        className="relative col-span-2 mb-10 aspect-square w-full drop-shadow-md"
+      >
+        <Image
+          src={image}
+          fill="true"
+          className="rounded-md opacity-75 grayscale transition hover:opacity-100 hover:grayscale-0"
+          alt="Sketch preview."
+        />
+      </div>
+    </Link>
+  )
 }
--- pages/sketches.js
+++ pages/sketches.js
@@ -19,9 +19,10 @@ export default function Sketches({ sketchesData }) {
     <Layout>
       <div>
         <h1 className="mb-8 block text-2xl font-medium text-tangerine">Sketches</h1>
-        <div className="grid grid-cols-3 lg:grid-cols-6">
+        <div className="grid gap-2 grid-cols-2">
           {sketchesData.map(({ id, image, description }) => (
             <SketchPreview
+							className="sketch-preview"
               key={id}
               id={id}
               image={image}
