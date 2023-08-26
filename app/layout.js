649ccdab08b177c33ee5f23261c82878cd075664
dino <dinojoaocosta@gmail.com>
2023-08-26 21:21:32 +0100

    Start migration to app folder

---
+++ app/layout.js
@@ -0,0 +1,42 @@
+import Link from '@/components/link'
+import PillButton from '@/components/pill_button'
+import Script from 'next/script'
+import { Analytics } from '@vercel/analytics/react'
+import { ArrowLongLeftIcon } from '@heroicons/react/24/solid'
+import { Metadata } from 'next';
+import { Open_Sans, Roboto } from 'next/font/google'
+import '../styles/globals.css'
+
+const font = Roboto({ subsets: ['latin'], weight: ['400', '700'] })
+
+export const metadata = {
+  title: 'dino.codes'
+}
+
+export default function RootLayout({ children }) {
+	return (
+		<html lang="en">
+			<body>
+				<div
+					className={`container max-w-3xl overflow-visible mx-auto px-4 ${font.className}`}
+					>
+					<Script strategy='afterInteractive' src='https://www.googletagmanager.com/gtag/js?id=G-LF9R8Y4KG2' />
+					<Script id='google-analytics' strategy='afterInteractive' dangerouslySetInnerHTML={{
+						__html: `
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-LF9R8Y4KG2', {
+page_path: window.location.pathname,
+});
+`,
+						}}
+					/>
+
+					<Analytics />
+					{children}
+				</div>
+			</body>
+		</html>
+	)
+}
--- app/page.js
+++ app/page.js
@@ -1,5 +1,4 @@
 import Link from 'next/link'
-import Layout from '@/components/layout'
 
 // List of links to be rendered in the index page.
 const links = [
@@ -10,21 +9,18 @@ const links = [
 
 export default function Home() {
   return (
-		<Layout>
 			<div className="flex h-screen">
 				<div className="m-auto text-center">
 					{links.map(({ href, text, animation }) => (
 						<div key={text} className={animation}>
 							<Link
 								href={href}
-								className="text-2xl font-bold transition-colors hover:text-orange-500 hover:dark:text-yellow-300"
-							>
+								className="text-2xl font-bold transition-colors hover:text-orange-500 hover:dark:text-yellow-300">
 								{text}
 							</Link>
 						</div>
 					))}
 				</div>
 			</div>
-		</Layout>
   )
 }
