befe5452dd169e477f70fbeff4fef98e505138c1
dino <dinojoaocosta@gmail.com>
2023-08-26 21:38:04 +0100

    Move posts page to app/

---
+++ app/posts/page.js
@@ -0,0 +1,33 @@
+import Link from 'next/link'
+import PageTitle from '@/components/page_title'
+import { getPostsData } from '@/lib/posts'
+
+export async function getData() {
+	const postsData = getPostsData()
+
+	return postsData
+}
+
+export default async function Posts() {
+	const postsData = await getData()
+
+	return (
+		<div>
+			<PageTitle title='Posts' />
+
+			{postsData.map(({ id, title, date, description }) => (
+				<Link
+					key={id}
+					className="mb-2 block rounded-md py-2 transition-colors"
+					href={`/posts/${id}`}
+					>
+					<h2 className="mb-2 text-xl">{title}</h2>
+					<small className="mb-1 block text-neutral-500">
+						{description}
+					</small>
+					<small className="block text-neutral-500">{date}</small>
+				</Link>
+			))}
+		</div>
+	)
+}
