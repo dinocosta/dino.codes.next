7efc4a05b4bc4a36f1cbe098547e9eab449f36f0
dino <dinojoaocosta@gmail.com>
2023-08-28 19:44:44 +0100

    Introduce constants module and update navbar

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { highlightColor, darkHighlightColor, textColor, darkTextColor } from '@/lib/constants'
 import Link from '@/components/link'
 import { CpuChipIcon, Bars3Icon } from '@heroicons/react/24/solid'
 
@@ -13,33 +14,33 @@ export default function NavBar() {
 	}
 
 	return (
-		<nav class="bg-gray-200 px-4 py-2 mb-2">
+		<nav class="bg-gray-200 dark:bg-neutral-900 px-4 py-4 mb-2">
 			<div class="mx-auto container">
 				<div class="flex justify-between">
 					<Link href="/">
-						<div class="bg-orange-500 w-6 h-6 rounded-full" />
+						<div class={`w-6 h-6 rounded-full bg-${highlightColor} dark:bg-${darkHighlightColor}`} />
 					</Link>
 
 					{/* Primary Navigation */}
 					<div class="flex items-center space-x-4 hidden md:flex">
-						<a href="/posts" class="hover:text-orange-500">Posts</a>
-						<a href="/sketches" class="hover:text-orange-500">Sketches</a>
-						<a href="/about" class="hover:text-orange-500">About</a>
+						<a href="/posts"		class={`hover:text-${highlightColor} hover:dark:text-${darkHighlightColor} transition-colors`}>Posts</a>
+						<a href="/sketches" class={`hover:text-${highlightColor} hover:dark:text-${darkHighlightColor} transition-colors`}>Sketches</a>
+						<a href="/about"		class={`hover:text-${highlightColor} hover:dark:text-${darkHighlightColor} transition-colors`}>About</a>
 					</div>
 
 					{/* Mobile Button */}
 					<div class="md:hidden flex items-center">
 						<button onClick={toggleMenu} class="mobile-menu-button">
-							<Bars3Icon className="h-6 w-6 fill-gray-900 dark:fill-yellow-800" />
+							<Bars3Icon className={`h-6 w-6 fill-${textColor} dark:${darkTextColor}`} />
 						</button>
 					</div>
 				</div>
 
 				{/* Mobile Menu */}
 				<div class="mobile-menu hidden md:hidden mt-2">
-					<a href="#" class="block py-2 text-sm hover:text-orange-500">Posts</a>
-					<a href="#" class="block py-2 text-sm hover:text-orange-500">Sketches</a>
-					<a href="#" class="block py-2 text-sm hover:text-orange-500">About</a>
+					<a href="#" class={`block py-2 hover:text-${highlightColor} hover:dark:text-${darkHighlightColor} transition-colors`}>Posts</a>
+					<a href="#" class={`block py-2 hover:text-${highlightColor} hover:dark:text-${darkHighlightColor} transition-colors`}>Sketches</a>
+					<a href="#" class={`block py-2 hover:text-${highlightColor} hover:dark:text-${darkHighlightColor} transition-colors`}>About</a>
 				</div>
 			</div>
 		</nav>
