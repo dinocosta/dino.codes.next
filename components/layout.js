660b118cb05812aaa488a62e12eb6503d5008b7f
dino <dinojoaocosta@gmail.com>
2023-09-05 19:51:58 +0100

    Update styles

--- components/layout.js
+++ components/layout.js
@@ -1,22 +1,15 @@
-import Head from 'next/head'
-import Link from '@/components/link'
 import NavBar from '@/components/navbar'
-import PillButton from '@/components/pill_button'
 import Script from 'next/script'
 import { Analytics } from '@vercel/analytics/react'
-import { ArrowLongLeftIcon } from '@heroicons/react/24/solid'
-import { IBM_Plex_Sans as Font } from 'next/font/google'
-import { useRouter } from 'next/router'
+import { Inter as Font } from 'next/font/google'
 
 const font = Font({ subsets: ['latin'], weight: ['400', '700'] })
 
 export default function Layout({ children }) {
 	return (
-		<div>
+		<div className={`${font.className}`}>
 			<NavBar />
-			<div
-				className={`px-4 overflow-visible ${font.className}`}
-				>
+			<div>
 				<Script strategy='afterInteractive' src='https://www.googletagmanager.com/gtag/js?id=G-LF9R8Y4KG2' />
 				<Script id='google-analytics' strategy='afterInteractive' dangerouslySetInnerHTML={{
 					__html: `
