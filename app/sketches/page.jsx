448b7f6f1661b71256d7307ab04f324505cf68b9
dinocosta <dinojoaocosta@gmail.com>
2024-02-04 21:23:12 +0000

    Experiment with bold text on page title

--- app/sketches/page.jsx
+++ app/sketches/page.jsx
@@ -4,32 +4,36 @@ import PageTitle from '@/components/page_title'
 import { getSketchesData } from '@/lib/sketches'
 
 export async function generateMetadata() {
-	return {
-		title: '[dino.codes] Sketches'
-	}
+  return {
+    title: '[dino.codes] Sketches',
+  }
 }
 
 export default async function Sketches() {
-	const sketchesData = await getSketchesData()
+  const sketchesData = await getSketchesData()
 
   return (
-		<div>
-			<PageTitle title='Sketches' />
+    <div>
+      <PageTitle title="Sketches" />
 
-			<p className="mb-4">
-				A collection of computer-generated images or animations built using <Link href="https://github.com/mattdesl/canvas-sketch">canvas-sketch</Link>.
-			</p>
+      <p className="mb-4">
+        A collection of computer-generated images or animations built using{' '}
+        <Link href="https://github.com/mattdesl/canvas-sketch">
+          canvas-sketch
+        </Link>
+        .
+      </p>
 
-			<div className="grid gap-2 grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
-				{sketchesData.map(({ id, image, description }) => (
-					<SketchPreview
-						className="sketch-preview"
-						key={id}
-						id={id}
-						image={image}
-					/>
-				))}
-			</div>
-		</div>
+      <div className="grid grid-cols-1 gap-2 md:grid-cols-2 lg:grid-cols-4">
+        {sketchesData.map(({ id, image, description }) => (
+          <SketchPreview
+            className="sketch-preview"
+            key={id}
+            id={id}
+            image={image}
+          />
+        ))}
+      </div>
+    </div>
   )
 }
